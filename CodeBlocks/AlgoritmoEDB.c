609866c63f095a756a8c9ea0bc88266235d07764
Marlon Dias <marlondias@outlook.com>
2014-06-30 12:07:18 -0300

    Pseudo codigo revisto + reinicio do EDB

--- CodeBlocks/AlgoritmoEDB.c
+++ CodeBlocks/AlgoritmoEDB.c
@@ -2,19 +2,36 @@
 
 #include "include/aleatorio.h"
 
-#define bit_full FFFFFFFFFFFFFFFF//todos os bits são 1
-#define bit_zeros 0000000000000000//todos os bits são 0
-
-
 //Recebe os parametros:
-#define D 64 //dimensão (variaveis do elemento)
-#define N 20 //número de indivíduos
+#define D 64 //dimensão (número de variaveis dentro de cada indivíduo, genes)
+#define N 20 //número de indivíduos na população
 #define PM 0.5 //taxa de mutação
 #define PR 0.5 //taxa de perturbação
 
 
+typedef struct {
+    uint64_t dna; //cada bit deste valor representa uma opção a ser ativada ou não
+    uint64_t tempoExecucao; //tempo para o programa X ser executado, após utilizar o valor DNA na compilação
+} individuo_t;
 
+int verificarValidadeDoCodigo(char* codigoAlvo){
+    //verificar se existe
+    //verificar se pode ser compilado normalmente pelo gcc
+    //retornar 0 ou 1
+    return 0;
+}
 
+void gerarPopulacaoAleatoria(individuo_t individuos[], int maxIndividuos){
+    for (int i=0; i<maxIndividuos; i++){
+        individuos[i].dna = randomUnsignedInt64(); //coloca DNAs aleatorios nos individuos
+    }
+}
+
+void calcularTempoDeTodos(individuo_t individuos[], int maxIndividuos, char* arquivoAlvo){
+    for (int i=0; i<maxIndividuos; i++){
+        individuos[i].tempoExecucao = Fitness(arquivoAlvo, individuos[i].dna); //manda o código e o DNA, recebe o tempo de execução
+    }
+}
 
 
 void Crossover(uint64_t individuoA, uint64_t individuoB){
@@ -27,65 +44,25 @@ void Mutacao(uint64_t individuo){
     return;
 }
 
-int Fit(uint64_t individuo){
-    //todo
-    return 33;
+
+void EDB(char* codigoAlvo, ){
+
+
+    individuo populacao[N]; //abre espaço para N individuos
+
+
+
+
 }
 
-void FitTodos(uint64_t individuos[]){
-    //todo
-    return;
-}
 
 
-void EDB() {
-
-    ///Gera população inicial com N indivíduos aleatorios
-    uint64_t individuos[N];
-    for (int a=0; a<N; a++){
-        individuos[a] = randomUnsignedInt64();
+int main(){
+    initSeed(); //inicia seed de aleatório
+    if (verificarValidadeDoCodigo("blah.c") == 1){
+        EDB(); //execute por X minutos
     }
-
-
-    ///calcular FITNESS de cada individuo -- falta
-
-
-    int parar = 0;
-    while (!parar) {
-
-        for (int i=0; i<N; i++){ //passa por cada individuo
-
-            int s = randomInt_RangeExceto(0, N-1, i); //escolhe um individuo qualquer, que não seja o atual
-
-
-            ///escolha aleatoriamente o indice de uma variavel (bit): j[rand] = rndint(1,D)
-            int bit = randomInt_Range(0, D-1); //escolhe aleatoriamente um dos bits (0 a 63)
-            //Wut??
-
-
-            uint64_t indivTeste = individuos[i]; //solução teste y recebe x[i]
-
-            for (int j=0; j<D; j++) {
-                if ((randomReal_Range(0, 1) < PR) || (0 /* j == j[rand] */)){ //perturbação de y
-                    if (randomReal_Range(0, 1) < PM){ //mutação
-                        //InverterBit()
-                    }
-                    else { //crossover
-                        //y[j] = x[sj] -- ??
-                    }
-                }
-            }
-
-            ///Calcular fitness de y -- será calculado no proximo IF
-
-            if (Fit(indivTeste) > Fit(individuos[i])){
-                individuos[i] = indivTeste; //atualiza solução corrente
-            }
-        }
-
-        ///encontrar melhor solução corrente de x[]
+    else {
+        return 1;
     }
-
-    ///apresentar resultados
-
 }
